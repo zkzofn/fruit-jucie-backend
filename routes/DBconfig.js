8accb27dc917b6e964f20219c98e8df074f9178a
Jangho Lee <zkzofn@gmail.com>
2017-09-14 16:44:28 +0900

    Edit DB config

--- routes/DBconfig.js
+++ routes/DBconfig.js
@@ -2,8 +2,8 @@ import mysql from 'mysql';
 
 export const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "rhwkdtns",
+  host: "eatmoregreen.cjttdb1qo9w9.ap-northeast-2.rds.amazonaws.com",
+  user: "eatmoregreen",
+  password: "dlwkdgh1!",
   database: "fruit"
 });
