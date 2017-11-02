c7b45006c1f42a910b0a803eb02575cc38c09fe3
Jangho Lee <zkzofn@gmail.com>
2017-11-02 18:10:16 +0900

    Editting order

--- routes/order.js
+++ routes/order.js
@@ -18,97 +18,94 @@ router.get("/", (req, res, next) => {
       })
   })
 }).post("/", (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) throw err;
-
-    const data = req.body;
-    const orderKeys = Object.keys(data).filter(key => {
-      return key !== "items"
-    });
-    const { items } = data;
-
-    connection.beginTransaction(err => {
+  try {
+    pool.getConnection((err, connection) => {
       if (err) throw err;
 
-      new Promise((resolve, reject) => {
-        let orderKeyString = "";
-        let orderValueString = "";
-
-        orderKeys.forEach((key) => {
-          orderKeyString += key + ", ";
-
-          // if (key === "user_id" || key === "status" || key === "total_price")
-          //   orderValueString += `${data[key]}, `;
-          // else
-          //  // for string values
-          orderValueString += `"${data[key]}", `;
-        });
-
-        orderKeyString += "date";
-        orderValueString += "now()";
-
-        // cart_detail 의 status 바꿔줘야해
-        // order_detail에 cart_detail에 있는 내용들 넣어줘야해
-
-        const query =
-          `INSERT INTO \`order\`
-                (${orderKeyString})
-         VALUES (${orderValueString})`;
-
-        queryConductor(connection, query)
-          .then((results) => {
-            resolve({results})
-          }).catch(err => {
-            connection.rollback(() => {
-              throw err
-            })
-          })
-      }).then(({results}) => {
-        const orderId = results.insertId;
-
-        const query =
-          `INSERT INTO order_detail
-                (order_id, product_id, product_option_id, count)
-                SELECT "${orderId}", product_id, product_option_id, count
-                  FORM cart_detail
-                 WHERE user_id = ${data.user_id}
-                   AND status = 0`;
-
-        return queryConductor(connection, query)
-          .catch(err => {
-            connection.rollback(() => {
-              throw err
-            })
-          })
-      }).then(() => {
-        const query =
-          `UPDATE cart_detail
-            SET status = 1
-          WHERE user_id = ${data.user_id}
-            AND status = 0`;
-
-        return queryConductor(connection, query)
-          .then(() => {
-            connection.commit(err => {
-              if (err)
-                connection.rollback(() => {
-                  throw err;
-                })
-            })
-          }).catch(err => {
-            connection.rollback(() => {
-              throw err;
-            })
-          })
-      }).then(() => {
-        console.log("Post order success");
-        connection.release();
-      }).catch(err => {
-        if (err) throw err;
-        connection.release();
+      const data = req.body;
+      const orderKeys = Object.keys(data).filter(key => {
+        return key !== "items"
       });
+
+      connection.beginTransaction(err => {
+        if (err) throw err;
+
+        new Promise((resolve, reject) => {
+          let orderKeyString = "";
+          let orderValueString = "";
+
+          orderKeys.forEach((key) => {
+            orderKeyString += key + ", ";
+            orderValueString += `"${data[key]}", `;
+          });
+
+          orderKeyString += "date";
+          orderValueString += "now()";
+
+          const query =
+            `INSERT INTO \`order\`
+                    (${orderKeyString})
+             VALUES (${orderValueString})`;
+
+          queryConductor(connection, query)
+            .then(results => {
+              const orderId = results.insertId;
+              const query =
+                `INSERT INTO order_detail
+                      (order_id, product_id, product_option_id, count)
+                      SELECT ${orderId}, product_id, product_option_id, count
+                        FROM cart_detail
+                       WHERE user_id = ${data.user_id}
+                         AND status = 0`;
+
+              queryConductor(connection, query)
+                .then(() => {
+                  const query =
+                    `UPDATE cart_detail
+                        SET status = 1
+                      WHERE user_id = ${data.user_id}
+                        AND status = 0`;
+
+                  queryConductor(connection, query)
+                    .then(() => {
+                      connection.commit(err => {
+                        if (err) {
+                          console.log("error 4")
+                          connection.rollback(() => {
+                            throw err;
+                          })
+                        }
+                        console.log(`user id = ${data.user_id} postOrder success`);
+                        connection.release();
+                      })
+                    }, err => {
+                      console.log("Error occurs while UPDATE cart_detail information in postOrder.");
+                      connection.rollback(() => {
+                        throw err;
+                      })
+                    })
+                }, err => {
+                  console.log("Error occurs while INSERT INTO order_detail information in postOrder.");
+                  connection.rollback(() => {
+                    throw err
+                  })
+                })
+            }, err => {
+              console.log("Error occurs while INSERT INTO order information in postOrder.");
+              connection.rollback(() => {
+                throw err
+              })
+            })
+        })
+      })
     })
-  })
+  } catch (err) {
+    console.log(err);
+    throw err;
+  } finally {
+    res.json({});
+    connection.release();
+  }
 });
 
 module.exports = router;
