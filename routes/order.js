ffe6415ce856eca54f6b63fdd874cbe9f3eeb8a7
Jangho Lee <ljh-sys@hanmail.net>
2018-04-16 09:18:15 +0900

    Edit order fromCart or not

--- routes/order.js
+++ routes/order.js
@@ -25,6 +25,7 @@ router.get("/", (req, res, next) => {
       const orderKeys = Object.keys(data).filter(key => {
         return key !== "items"
       });
+      const { fromCart } = data;
 
       connection.beginTransaction(err => {
         if (err) throw err;
@@ -50,7 +51,6 @@ router.get("/", (req, res, next) => {
         queryConductor(connection, query).then(results => {
           const orderId = results.insertId;
 
-          // order_detail 에 cart에서 구매하는거 아니더라도 정보넣어줘야해
           const orderDetailValues = data.items.map(item => {
             const { product, options } = item;
 
@@ -85,25 +85,44 @@ router.get("/", (req, res, next) => {
             }
           }).join(", ");
 
-          // cart_detail에서 가져와서 넣는게 아니라 그냥 넣어줘야해
           const query = `
           INSERT INTO order_detail
                  (order_id, product_id, product_option_id, count, days, mon, tue, wed, thur, fri)
           VALUES ${orderDetailValues}`;
 
-          console.log(query);
 
           queryConductor(connection, query).then(() => {
-            const query = `
-            UPDATE cart_detail
-               SET status = 1
-             WHERE user_id = ${data.user_id}
-               AND status = 0`;
+            if (fromCart) {
+              const query = `
+             UPDATE cart_detail
+                SET status = 1
+              WHERE user_id = ${data.user_id}
+                AND status = 0`;
 
-            queryConductor(connection, query).then(() => {
+              queryConductor(connection, query).then(() => {
+                connection.commit(err => {
+                  if (err) {
+                    console.log("Error occurs while COMMIT in postOrder");
+                    connection.rollback(() => {
+                      connection.release();
+                      throw err;
+                    })
+                  }
+                  console.log(`user id = ${data.user_id} postOrder success`);
+                  connection.release();
+                  res.end();
+                })
+              }, err => {
+                console.log("Error occurs while UPDATE cart_detail information in postOrder.");
+                connection.rollback(() => {
+                  connection.release();
+                  throw err;
+                })
+              })
+            } else {
               connection.commit(err => {
                 if (err) {
-                  console.log("Error ccurs while COMMIT in postOrder");
+                  console.log("Error occurs while COMMIT in postOrder");
                   connection.rollback(() => {
                     connection.release();
                     throw err;
@@ -113,13 +132,7 @@ router.get("/", (req, res, next) => {
                 connection.release();
                 res.end();
               })
-            }, err => {
-              console.log("Error occurs while UPDATE cart_detail information in postOrder.");
-              connection.rollback(() => {
-                connection.release();
-                throw err;
-              })
-            })
+            }
           }, err => {
             console.log("Error occurs while INSERT INTO order_detail information in postOrder.");
             connection.rollback(() => {
