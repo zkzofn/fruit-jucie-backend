29afe5329db59c99a85bc6ab3f93b6f82bc7a182
Jangho Lee <ljh-sys@hanmail.net>
2018-09-05 14:32:35 +0900

    Edit comment to sessionTimeout

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 import crypto from 'crypto-js';
 
 export const secretToken = "fRu1t_token_privacy";
-export const sessionTimeout = 60 * 60;
+export const sessionTimeout = 60 * 60;  // 1 hour
 
 export const makeSessionKey = (userInfoString) => {
   return crypto.AES.encrypt(userInfoString, secretToken).toString();
